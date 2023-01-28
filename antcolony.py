cd1067d8e6b520f55034a7cf5549c5d794ec513f
Stas <stasselin1980@gmail.com>
2023-01-28 20:02:32 +0900

    NotWorkingBeta-0.0.1

--- antcolony.py
+++ antcolony.py
@@ -3,10 +3,22 @@ class Point():
         self.x, self.y = x,y
         self.edges = list()
 
-    def add_edge(self, point):
-        edge = Edge(self,point)
-        self.edges.append(edge)
-        point.edges.append(edge)
+    def link(self, point):
+        is_empty = True
+        for i in self.edges:
+            i = i.points
+            if self in i and point in i:
+                is_empty = False
+        for i in point.edges:
+            i = i.points
+            if self in i and point in i:
+                is_empty = False
+        if is_empty:
+            edge = Edge(self, point)
+            self.edges.append(edge)
+            point.edges.append(edge)
+
+
 
     def __repr__(self):
         return f'({self.x:.1f},{self.y:.1f})'
@@ -16,14 +28,8 @@ class Edge():
         self.pher = pheramon
         self.points = (point1, point2)
         self.distance = ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
-        self.t = 0.
-
-    def add(self, value):
-        self.t += value
-
     def go(self, coof):
-        self.pher = coof * self.pher + self.t
-        self.t = 0
+        self.pher = coof * self.pher
 
 class Ant():
     def __init__(self, point:Point):
@@ -36,17 +42,24 @@ class Ant():
         pass
 
 class AntColony():
-    def __init__(self, points : list):
+    def __init__(self, points : list, make_links = True):
         self.points = points
+        if make_links:
+            self.link_points()
 
     def add_point(self, point:Point):
         self.points+=point
+
+    def link_points(self):
+        for i in self.points:
+            for k in self.points:
+                i.link(k)
 
     def add_ant(self):
         if self.points:
             self.ant = Ant(point= self.points[0])
 
-    def change_ant_point_to_point(self, point:Point):
+    def change_ant_position_to_point(self, point:Point):
         self.ant.change_point(point)
 
     def go(self, iters, DIS_COOF, Q, A, B, show_result = True):
