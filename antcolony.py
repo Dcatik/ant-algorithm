6902cfc53b8f252c39f74a339ca1e2ba783d8ffe
Stas <stasselin1980@gmail.com>
2023-02-03 00:05:25 +0900

    Beta 0.0.2 Added: +data checking for Go method

--- antcolony.py
+++ antcolony.py
@@ -83,7 +83,16 @@ class Ant():
 
 class AntColony():
     def __init__(self, points : list, make_links = True):
-        self.points = points
+        temp = points.copy()
+        i = 0
+        while i < len(temp):
+            if not type(temp[i]) is Point:
+                print('Type other than Point encountered in array')
+                temp.pop(i)
+                i-=1
+            else:
+                i+=1
+        self.points = temp
         if make_links:
             self.link_points()
         self.add_ant()
@@ -101,14 +110,51 @@ class AntColony():
         if self.points:
             self.ant = Ant(point= self.points[0])
 
-    def go(self,ants, iters, DIS_COOF, Q, A, B, show_result = True):
+    def go(self, ants_count, iters, DIS_COOF, Q, A, B, show_result = True):
+        # data checking:
+        # types checking
+        values = [ants_count, iters, DIS_COOF, Q, A, B]
+        valid_types = [int, float]
+        if show_result not in [False, True, int]:
+            print('Invalid Bool Type Detected')
+            show_result = True
+        for i in range(len(values)):
+            if type(values[i]) not in valid_types:
+                print('Invalid Type Detected')
+                try:
+                    values[i] = int(values[i])
+                except:
+                    values[i] = 1
+        ants_count, iters, DIS_COOF, Q, A, B = values
+
+        # values checking
+        if 100 < ants_count or ants_count < 0:
+            print('Too many ants')
+            ants_count = 10
+        if 100 < iters or iters < 0:
+            print('Too many iterations')
+            iters = 10
+        if 1.< DIS_COOF or DIS_COOF < 0:
+            print('Incorrect evaporative coefficient')
+            DIS_COOF = 0.36
+        if 100 < Q or Q < 0:
+            print('Too large or too small value for the pheramon accrual constant')
+            Q = 0.5
+        if 100 < A or A < -100:
+            print('Too big or too small value for dependency constant')
+            A = 2
+        if 100 < B or B < -100:
+            print('Too big or too small value for dependency constant')
+            B = 4
+
+        # ant algorithm:
         minimal_way = (1000000000000, None)
         edges = set()
         for i in self.points:
             edges.update(i.edges)
         for i in range(iters):
             ant_results = list()
-            for k in range(ants):
+            for k in range(ants_count):
                 ant_results.append(self.ant.go(a=A, b=B, points=self.points, start_point=self.points[random.randrange(len(self.points))]))
                 self.ant.distance = 0
             # убираем с каждой дорожки некий коэффициент DIS_COOF
@@ -130,5 +176,6 @@ class AntColony():
             p1, p2 = i.points[0], i.points[1]
             plt.plot([p1.x, p2.x],[p1.y,p2.y], c = 'gray')
         plt.text(100, 100, minimal_way[0])
+        print(values)
         plt.show()
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 from antcolony import AntColony, Point
 
-points = [Point(100 * random.random(), 100 * random.random()) for i in range(100)]
+points = [Point(100 * random.random(), 100 * random.random()) for i in range(24)] + ['abpb']
 colony = AntColony(points)
-colony.go(ants=10,iters=50, DIS_COOF=0.36, A=2, B=4, Q=0.5)
+colony.go(ants_count='abpba',iters=5, DIS_COOF=0.36, A=2, B=4, Q=0.5)
